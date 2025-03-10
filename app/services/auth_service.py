0598cfe39f556291775df2f42afffe8f5856ba6d
valentinlamine <github@valentinlamine.fr>
2025-03-10 09:35:54 +0100

    adding authen with token

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -1,20 +1,21 @@
 # app/services/auth_service.py
-import jwt
 import datetime
 import os
-from passlib.context import CryptContext
-from app.storage.user_storage import UserStorage
-from app.models.user import User, SimplifiedUser
-from dotenv import load_dotenv
-import time
 
-load_dotenv()
+import jwt
+from fastapi.security import OAuth2PasswordBearer
+from passlib.context import CryptContext
+
+from app.models.user import User, SimplifiedUser
+from app.storage.user_storage import UserStorage
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 SECRET_KEY = os.getenv("SECRET_KEY", "supersecretkey")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
 class AuthService:
@@ -52,9 +53,33 @@ class AuthService:
             return None, "L'utilisateur n'existe pas"
 
         if not pwd_context.verify(user.password, stored_user["password"]):
-            return None, "Identifiants incorrect"
+            return None, "Identifiants incorrects"
 
-        if stored_user and stored_user['password'] == pwd_context.hash(user.password):
-            # Générer un token JWT ou une valeur aléatoire pour simuler un token
-            return "fake-jwt-token"
-        return None
+        # Génération du token JWT
+        access_token = AuthService.create_access_token({"sub": user.username})
+        return access_token, "Connexion réussie"
+
+    @staticmethod
+    def create_access_token(data: dict):
+        """
+        Génère un token JWT avec une date d'expiration.
+        """
+        to_encode = data.copy()
+        expire = datetime.datetime.utcnow() + datetime.timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+        to_encode.update({"exp": expire})
+
+        return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+    @staticmethod
+    def verify_token(token: str):
+        """
+        Vérifie si un token est valide.
+        """
+        try:
+            payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+            username: str = payload.get("sub")
+            if username is None:
+                return None
+            return username
+        except jwt.ExpiredSignatureError:
+            return None, "Token expiré"
